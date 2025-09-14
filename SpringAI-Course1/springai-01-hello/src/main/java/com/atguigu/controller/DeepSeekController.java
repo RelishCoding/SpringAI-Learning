a72fbde02607e10341e2120c81d4ab8428917f3c
qzh <2806894439@qq.com>
2025-09-15 00:26:39 +0800

    SpringAI快速入门

---
+++ SpringAI-Course1/springai-01-hello/src/main/java/com/atguigu/controller/DeepSeekController.java
@@ -0,0 +1,22 @@
+package com.atguigu.controller;
+
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeepSeekController {
+    @Autowired
+    // private OpenAiChatModel chatModel;
+    private ChatModel chatModel;
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "message", defaultValue = "hello") String message) {
+        String result = chatModel.call(message);
+        System.out.println(result);
+        return result;
+    }
+}
