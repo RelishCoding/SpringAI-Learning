c7e4b289a190cdc9227254106efda75fae019f13
qzh <2806894439@qq.com>
2026-02-10 23:23:17 +0800

    创建自定义Function

---
+++ SpringAI-Course1/springai_03_function/src/main/java/com/atguigu/function/config/CalculatorService.java
@@ -0,0 +1,33 @@
+package com.atguigu.function.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+
+import java.util.function.Function;
+
+@Configuration
+public class CalculatorService {
+    public record AddOperation(int a, int b) {
+
+    }
+
+    public record MulOperation(int m, int n) {
+
+    }
+
+    // 注册
+    @Bean
+    @Description("加法运算")
+    public Function<AddOperation, Integer> addOperation() {
+        return request -> {
+            return request.a + request.b;
+        };
+    }
+
+    @Bean
+    @Description("乘法运算")
+    public Function<MulOperation, Integer> mulOperation() {
+        return request -> request.m * request.n;
+    }
+}
