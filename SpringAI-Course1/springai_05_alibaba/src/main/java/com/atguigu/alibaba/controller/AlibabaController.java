04c24a1cb2b6f08322aa6642beba8b00afca52b9
qzh <2806894439@qq.com>
2026-02-11 19:14:00 +0800

    SpringAI Alibaba快速入门

---
+++ SpringAI-Course1/springai_05_alibaba/src/main/java/com/atguigu/alibaba/controller/AlibabaController.java
@@ -0,0 +1,51 @@
+package com.atguigu.alibaba.controller;
+
+import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatOptions;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class AlibabaController {
+    private static final String DEFAULT_PROMPT = "你是一个博学的智能聊天助手，请根据用户提问回答！";
+
+    private final ChatClient dashScopeChatClient;
+
+    public AlibabaController(ChatClient.Builder chatClientBuilder) {
+        this.dashScopeChatClient = chatClientBuilder
+                .defaultSystem(DEFAULT_PROMPT)
+                .defaultAdvisors(
+                        MessageChatMemoryAdvisor.builder(MessageWindowChatMemory.builder().build()).build())
+                .defaultAdvisors(new SimpleLoggerAdvisor())
+                // 设置 ChatClient 中 ChatModel 的 Options 参数
+                .defaultOptions(
+                        DashScopeChatOptions.builder()
+                                .topP(0.7)
+                                .build()
+                )
+                .build();
+    }
+
+    /**
+     * ChatClient 简单调用
+     */
+    @GetMapping("/simple/chat")
+    public String simpleChat(@RequestParam(value = "query", defaultValue = "hello") String query) {
+        return dashScopeChatClient.prompt(query).call().content();
+    }
+
+    /**
+     * ChatClient 流式调用
+     */
+    @GetMapping("/stream/chat")
+    public Flux<String> streamChat(@RequestParam(value = "query", defaultValue = "hello") String query, HttpServletResponse response) {
+        response.setCharacterEncoding("UTF-8");
+        return dashScopeChatClient.prompt(query).stream().content();
+    }
+}
