18e2f26241bb54651b7d2876f6c8fbbe2890f3cc
qzh <2806894439@qq.com>
2026-02-12 22:13:48 +0800

    启动类

---
+++ SpringAI-Course1/springai_08_case/src/main/java/com/atguigu/practice/SpringAiCaseApplication.java
@@ -0,0 +1,11 @@
+package com.atguigu.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiCaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiCaseApplication.class, args);
+    }
+}
